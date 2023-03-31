ea155048426eb37efdb2895f614cff887bbb9659
ShiYang1101 <shiyanglee1998@gmail.com>
2023-03-31 16:01:03 +0100

    Converting linkedin scarper function into a package

---
+++ scraper/setup.py
@@ -0,0 +1,12 @@
+
+import setuptools
+from distutils.core import setup
+
+setup(name='scraper',
+      version='1.0',
+      description='Linkedin scraper package',
+      author='Aetius Lee',
+      author_email='shiyanglee1998@gmail.com',
+      url='',
+      packages=setuptools.find_packages()
+     )
